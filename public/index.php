3fc838aa51acaf6b451c3c9ecc33ee7a7879a973
Herve Tusse <HerveTusse@gmail.com>
2018-09-20 23:17:21 +0200

    see response

--- public/index.php
+++ public/index.php
@@ -19,75 +19,6 @@ $app->add(new Tuupola\Middleware\HttpBasicAuthentication([
     ]
 ]));
 
-/* 
-    endpoint: fetchCurrencyRate
-    parameters: currency, symbol
-    method: POST
-*/
-$app->post('/fetchCurrencyRate', function(Request $request, Response $response){
-
-    if(!haveEmptyParameters(array('currency', 'symbol'), $request, $response)){
-        $request_data = $request->getParsedBody(); 
-        $json = json_decode($request_data);
-        $text = $json->result->resolvedQuery;
-
-        return "You said: " . $text . " | I found Intent: " . $intent;
-
-        $symbol = $request_data['symbol'];
-        $currency = $request_data['currency'];
-
-
-        try {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, "https://ratesapi.io/api/latest?base=$currency&symbols=$symbol");
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_TCP_KEEPALIVE, 1);
-            curl_setopt($ch, CURLOPT_TCP_KEEPIDLE, 2);
-            $data = curl_exec($ch);
-            if(curl_errno($ch)){
-                throw new Exception(curl_error($ch));
-            }
-            curl_close($ch);
-
-            $response->write($data);
-
-            return $response
-                        ->withHeader('Content-type', 'application/json')
-                        ->withStatus(201);
-          } catch(Exception $e) {
-            $response->write(json_encode(curl_error($ch)));
-            // do something on exception
-          }
-    }
-    return $response
-        ->withHeader('Content-type', 'application/json')
-        ->withStatus(422);    
-});
-
-
-
-function haveEmptyParameters($required_params, $request, $response){
-    $error = false; 
-    $error_params = '';
-    $request_params = $request->getParsedBody(); 
-
-    foreach($required_params as $param){
-        if(!isset($request_params[$param]) || strlen($request_params[$param])<=0){
-            $error = true; 
-            $error_params .= $param . ', ';
-        }
-    }
-
-    if($error){
-        $error_detail = array();
-        $error_detail['error'] = true; 
-        $error_detail['message'] = 'Required parameters ' . substr($error_params, 0, -2) . ' are missing or empty';
-        $response->write(json_encode($error_detail));
-    }
-    return $error; 
-}
-
-$app->run();
 
 /* 
     endpoint: fetchCurrencyRate
@@ -110,6 +41,7 @@ $app->post('/fetchCurrencyRate', function(Request $request, Response $response){
         $response->displayText = $responseText;
         $response->source = "webhook";
         echo json_encode($response);
+        exit;
         $symbol = $request_data['symbol'];
         $currency = $request_data['currency'];
 
