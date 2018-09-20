b262b379d2cb8aaafd2ee836da49b4362935035a
Herve Tusse <HerveTusse@gmail.com>
2018-09-21 00:49:38 +0200

    see response

--- public/index.php
+++ public/index.php
@@ -88,7 +88,7 @@ function prepareResponse($text, $currency, $amt){
         // do something on exception
         }
     $rate = json_decode($data);
-    var_dump($rate);
+    var_dump(json_decode($rate["rates"]));
 
     exit;
 }
