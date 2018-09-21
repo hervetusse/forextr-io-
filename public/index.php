1be88c0e0d536d865e74726fa73b3d677be85243
Herve Tusse <HerveTusse@gmail.com>
2018-09-21 06:51:52 +0200

    cleanup

--- public/index.php
+++ public/index.php
@@ -22,7 +22,6 @@ $app->add(new Tuupola\Middleware\HttpBasicAuthentication([
 
 /* 
     endpoint: fetchCurrencyRate
-    parameters: currency, symbol
     method: POST
 */
 $app->post('/fetchCurrencyRate', function(Request $request, Response $response){
@@ -42,29 +41,6 @@ $app->post('/fetchCurrencyRate', function(Request $request, Response $response){
     $response->source = "webhook";
     return json_encode($response);
 });
-
-
-
-function haveEmptyParameters($required_params, $request, $response){
-    $error = false; 
-    $error_params = '';
-    $request_params = $request->getParsedBody(); 
-
-    foreach($required_params as $param){
-        if(!isset($request_params[$param]) || strlen($request_params[$param])<=0){
-            $error = true; 
-            $error_params .= $param . ', ';
-        }
-    }
-
-    if($error){
-        $error_detail = array();
-        $error_detail['error'] = true; 
-        $error_detail['message'] = 'Required parameters ' . substr($error_params, 0, -2) . ' are missing or empty';
-        $response->write(json_encode($error_detail));
-    }
-    return $error; 
-}
 
 function prepareResponse($text, $currency, $amt){
 
